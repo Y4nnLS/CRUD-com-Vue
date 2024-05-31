143bf0302c118481a40d5b2fead01ecbfeb94594
Yann Lucas <101208372+Y4nnLS@users.noreply.github.com>
2024-05-31 11:23:46 -0300

    a

--- code-with-quarkus/src/main/java/org/acme/CORSFilter.java
+++ code-with-quarkus/src/main/java/org/acme/CORSFilter.java
@@ -3,7 +3,6 @@ package org.acme;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
-import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
@@ -11,10 +10,11 @@ public class CORSFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
-        headers.add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
     }
 }
+
--- quarkus-vue/src/services/api.js
+++ quarkus-vue/src/services/api.js
@@ -1,8 +1,7 @@
-// src/services/api.js
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api', // Altere para o URL da sua API
+  baseURL: 'http://localhost:8080', // Mantenha o mesmo URL da sua API
   headers: {
     'Content-Type': 'application/json',
   },
@@ -10,18 +9,18 @@ const apiClient = axios.create({
 
 export default {
   getMovies(params) {
-    return apiClient.get('/movies', { params });
+    return apiClient.get('/movies', { params }); // Alterado para '/movies'
   },
   getMovie(id) {
-    return apiClient.get(`/movies/${id}`);
+    return apiClient.get(`/movies/${id}`); // Alterado para '/movies/:id'
   },
   createMovie(movie) {
-    return apiClient.post('/movies', movie);
+    return apiClient.post('/movies/add', movie); // Alterado para '/movies/add'
   },
   updateMovie(id, movie) {
-    return apiClient.put(`/movies/${id}`, movie);
+    return apiClient.put(`/movies/${id}/update`, movie); // Alterado para '/movies/:id/update'
   },
   deleteMovie(id) {
-    return apiClient.delete(`/movies/${id}`);
+    return apiClient.delete(`/movies/${id}/delete`); // Alterado para '/movies/:id/delete'
   },
 };
